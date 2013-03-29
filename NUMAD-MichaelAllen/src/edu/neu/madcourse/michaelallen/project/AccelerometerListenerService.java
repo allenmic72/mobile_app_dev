4712b0ecaf0f543580749a729476d1bfca8c5cc4
Michael Allen <allen.mic@husky.neu.edu>
2013-03-29 18:00:55 -0400

    put in apple's accelerometer filter

--- NUMAD-MichaelAllen/src/edu/neu/madcourse/michaelallen/project/AccelerometerListenerService.java
+++ NUMAD-MichaelAllen/src/edu/neu/madcourse/michaelallen/project/AccelerometerListenerService.java
@@ -23,6 +23,12 @@ public class AccelerometerListenerService extends Service implements SensorEvent
 	float z;
 	long lastEventTime = 0;
 	
+	float runningAverage;
+	int eventCount;
+	float[] priorX;
+	float[] priorY;
+	float[] priorZ;
+	
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		
@@ -36,6 +42,11 @@ public class AccelerometerListenerService extends Service implements SensorEvent
 		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 		mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 		mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+		
+		priorX = new float[50];
+		priorY = new float[50];
+		priorZ = new float[50];
+		eventCount = 0;
 		
 	}
 	
@@ -59,14 +70,64 @@ public class AccelerometerListenerService extends Service implements SensorEvent
 
 	@Override
 	public void onSensorChanged(SensorEvent ev) {
-		x = ev.values[0];
-		y = ev.values[1];
-		z = ev.values[2];
+		highPassFilter(ev.values[0], ev.values[1], ev.values[2]);
 		
 		
 		
-		Log.d(TAG, Calendar.getInstance().getTime().getTime() - lastEventTime + " at:: " + x + " " + y + " " + z);
 		
+	}
+	
+	private static final boolean ADAPTIVE_ACCEL_FILTER = true;
+	float lastAccel[] = new float[3];
+	float accelFilter[] = new float[3];
+
+	/**
+	 * Adaptation of Apple's high pass filter
+	 * Gotta be good since it's an apple product
+	 * @param accelX
+	 * @param accelY
+	 * @param accelZ
+	 */
+	public void highPassFilter(float accelX, float accelY, float accelZ) {
+	    // high pass filter
+	    float updateFreq = 30; // match this to your update speed
+	    float cutOffFreq = 1.0f;
+	    float RC = 1.0f / cutOffFreq;
+	    float dt = 1.0f / updateFreq;
+	    float filterConstant = RC / (dt + RC);
+	    float alpha = filterConstant; 
+	    float kAccelerometerMinStep = 0.033f;
+	    float kAccelerometerNoiseAttenuation = 3.0f;
+
+	    if(ADAPTIVE_ACCEL_FILTER)
+	    {
+	        float d = clamp(Math.abs(norm(accelFilter[0], accelFilter[1], accelFilter[2]) - norm(accelX, accelY, accelZ)) / kAccelerometerMinStep - 1.0f, 0.0f, 1.0f);
+	        alpha = d * filterConstant / kAccelerometerNoiseAttenuation + (1.0f - d) * filterConstant;
+	    }
+
+	    accelFilter[0] = (float) (alpha * (accelFilter[0] + accelX - lastAccel[0]));
+	    accelFilter[1] = (float) (alpha * (accelFilter[1] + accelY - lastAccel[1]));
+	    accelFilter[2] = (float) (alpha * (accelFilter[2] + accelZ - lastAccel[2]));
+
+	    lastAccel[0] = accelX;
+	    lastAccel[1] = accelY;
+	    lastAccel[2] = accelZ;
+	    
+	    Log.d(TAG, Calendar.getInstance().getTime().getTime() - lastEventTime + " at:: " +  
+	    		accelFilter[0] + " " + accelFilter[1] + " " + accelFilter[2]);
+	}
+	
+	private float norm(float x, float y, float z){
+		 return (float) Math.sqrt(x * x + y * y + z * z);
+	}
+	
+	private float clamp(float v, float min, float max){
+		if(v > max)
+	        return max;
+	    else if(v < min)
+	        return min;
+	    else
+	        return v;
 	}
 	
 	
